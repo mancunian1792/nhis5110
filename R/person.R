05d733df6984447e9a89d1979659a4a53924d018
mancunian1792 <linkinnation1792@gmail.com>
2018-11-26 18:40:43 -0500

    Data and its corresponding documentation added.

---
+++ R/person.R
@@ -0,0 +1,13 @@
+#' Person
+#'
+#' Look up person data
+#'
+#' @source https://www.cdc.gov/nchs/nhis/nhis_2017_data_release.htm
+#' @format Data frame with columns
+#' \describe{
+#' \item{carrier}{Two letter abbreviation}
+#' \item{name}{Full name}
+#' }
+#' @examples
+#'   nhisPerson
+"nhisPerson"
