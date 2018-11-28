147d12572d5c29903fdc4b46865b8711d9cfd0a3
mancunian1792 <linkinnation1792@gmail.com>
2018-11-27 21:24:42 -0500

    Added Sample Adult data

---
+++ R/adult.R
@@ -0,0 +1,13 @@
+#' Sample Adult
+#'
+#' Look up adult data
+#'
+#' @source https://www.cdc.gov/nchs/nhis/nhis_2017_data_release.htm
+#' @format Data frame with columns
+#' \describe{
+#' \item{carrier}{Two letter abbreviation}
+#' \item{name}{Full name}
+#' }
+#' @examples
+#'   sampleAdult
+"sampleAdult"
